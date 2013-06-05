8191ac785964a02f6677eb565cd50faaccd3ba0b
Eugene Gritskevich <qwaker.00@gmail.com>
2013-06-05 21:50:57 +0300

    Make dev chars

--- qwalculator.c
+++ qwalculator.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
+#include <linux/fs.h>
 #include <asm/uaccess.h>
 
 #define DRIVER_AUTHOR "Eugene Gritskevich <qwaker.00@gmail.com>"
@@ -21,37 +22,37 @@ MODULE_PARM_DESC(inp3, "Input file number 3");
 module_param(out, charp, 0000);
 MODULE_PARM_DESC(out, "Output file");
  
-static struct proc_dir_entry* proc_files[4] = {NULL, NULL, NULL, NULL};
-static char * proc_names[4] = {NULL, NULL, NULL, NULL};
+static int char_dev[4] = {0, 0, 0, 0};
+static char * char_names[4] = {NULL, NULL, NULL, NULL};
 
 static long var1 = 0;
 static long var2 = 0;
 static char op = '+';
 
-static size_t read_var(char *buffer,
-                       char **buffer_location,
-                       off_t offset, int buffer_length, int *eof, void *data)
+static ssize_t read_var1(struct file* fil,
+                        char* buffer,
+                        size_t length,
+                        loff_t* offset)
 {    
     long len;
-    long value = *(long*)data;
-    if (offset > 0)
-        return 0;
-    len = sprintf(buffer, "%ld\n", value);
+    len = sprintf(buffer, "%ld\n", var1);
     return len;
 }
 
-static size_t write_var(struct file* file, const char* buffer,
-                        unsigned long count, void* data)
+static ssize_t write_var1(struct file* fil,
+                         const char* buffer,
+                         size_t count,
+                         loff_t* offset)
 {
     char ubuffer[512];
 
     if (count > 511) count = 511;
     if (copy_from_user(ubuffer, buffer, count)) {
-        printk(KERN_ERR "Failed to write to proc fs\n");
+        printk(KERN_ERR "Failed to write to char dev\n");
         return -EFAULT;
     }
     ubuffer[count] = 0;
-    if (sscanf(ubuffer, "%ld", &*(long*)data) != 1) {
+    if (sscanf(ubuffer, "%ld", &*(long*)var1) != 1) {
         printk(KERN_ERR "Long value expected\n");
         return -EFAULT;
     }
@@ -59,26 +60,57 @@ static size_t write_var(struct file* file, const char* buffer,
     return count;
 }
 
-static size_t read_op(char *buffer,
-                      char **buffer_location,
-                      off_t offset, int buffer_length, int *eof, void *data)
+static ssize_t read_var2(struct file* fil,
+                        char* buffer,
+                        size_t length,
+                        loff_t* offset)
 {    
     long len;
-    char value = *(char*)data;
-    if (offset > 0)
-        return 0;
-    len = sprintf(buffer, "%c\n", value);
+    len = sprintf(buffer, "%ld\n", var2);
     return len;
 }
 
-static size_t write_op(struct file* file, const char* buffer,
-                       unsigned long count, void* data)
+static ssize_t write_var2(struct file* fil,
+                         const char* buffer,
+                         size_t count,
+                         loff_t* offset)
+{
+    char ubuffer[512];
+
+    if (count > 511) count = 511;
+    if (copy_from_user(ubuffer, buffer, count)) {
+        printk(KERN_ERR "Failed to write to char dev\n");
+        return -EFAULT;
+    }
+    ubuffer[count] = 0;
+    if (sscanf(ubuffer, "%ld", &*(long*)var2) != 1) {
+        printk(KERN_ERR "Long value expected\n");
+        return -EFAULT;
+    }
+
+    return count;
+}
+
+static size_t read_op(struct file* fil,
+                      char* buffer,
+                      size_t length,
+                      loff_t* offset)
+{    
+    long len;
+    len = sprintf(buffer, "%c\n", op);
+    return len;
+}
+
+static size_t write_op(struct file* fil,
+                       const char* buffer,
+                       size_t count,
+                       loff_t* offset)
 {
     char ubuffer[512], new_op;
 
     if (count > 511) count = 511;
     if (copy_from_user(ubuffer, buffer, count)) {
-        printk(KERN_ERR "Failed to write to proc fs\n");
+        printk(KERN_ERR "Failed to write to char dev\n");
         return -EFAULT;
     }
     new_op = ubuffer[0];
@@ -86,19 +118,17 @@ static size_t write_op(struct file* file, const char* buffer,
         printk(KERN_ERR "One of '+', '-', '/', '*' expected\n");
         return -EFAULT;
     }
-    *(char*)data = new_op;
+    op = new_op;
 
     return count;
 }
 
-static size_t read_result(char *buffer,
-                          char **buffer_location,
-                          off_t offset, int buffer_length, int *eof, void *data)
+static size_t read_result(struct file* fil,
+                          char *buffer,
+                          size_t length,
+                          loff_t* offset)
 {
     long len = 0;
-    if (offset > 0)
-        return 0;
-
     switch (op) {
         case '+': len = sprintf(buffer, "%ld\n", var1 + var2); break;
         case '-': len = sprintf(buffer, "%ld\n", var1 - var2); break;
@@ -109,45 +139,39 @@ static size_t read_result(char *buffer,
                 len = sprintf(buffer, "%ld\n", var1 / var2);
             break;
     }
-    
     return len;
 }
 
 static int __init init(void)
 {
     int i;
+    struct file_operations fops[4];
+    memset(fops, 0, sizeof(fops));
 
     printk(KERN_INFO "Trying to create proc files:\n");
 
-    proc_names[0] = inp1;
-    proc_names[1] = inp2;
-    proc_names[2] = inp3;
-    proc_names[3] = out;
+    char_names[0] = inp1;
+    char_names[1] = inp2;
+    char_names[2] = inp3;
+    char_names[3] = out;
 
+    fops[0].read = (void*)read_var1;
+    fops[0].write = (void*)write_var1;
+    fops[1].read = (void*)read_var2;
+    fops[1].write = (void*)write_var2;
+    fops[2].read = (void*)read_op;
+    fops[2].write = (void*)write_op;
+    fops[3].read = (void*)read_result;
+   
     for (i = 0; i < 4; ++i) {
-        proc_files[i] = create_proc_entry(proc_names[i], 0666, NULL);
-        if (proc_files[i] == NULL) {
-            printk(KERN_ERR "Failed to create /proc/%s", proc_names[i]);
+        char_dev[i] = register_chrdev(0, char_names[i], &fops[i]);
+        if (char_dev[i] < 0) {
+            printk(KERN_ERR "Failed to create /dev/%s", char_names[i]);
             return -ENOMEM;
         } else {
-            printk(KERN_INFO "/proc/%s\n", proc_names[i]);
+            printk(KERN_INFO "/dev/%s\n", char_names[i]);
         }
     }
-
-    proc_files[0]->read_proc = (void*)read_var;
-    proc_files[0]->write_proc = (void*)write_var;
-    proc_files[0]->data = &var1;
-
-    proc_files[1]->read_proc = (void*)read_var;
-    proc_files[1]->write_proc = (void*)write_var;
-    proc_files[1]->data = &var2;
-
-    proc_files[2]->read_proc = (void*)read_op;
-    proc_files[2]->write_proc = (void*)write_op;
-    proc_files[2]->data = &op;
-
-    proc_files[3]->read_proc = (void*)read_result;
- 
     return 0;
 }
 
@@ -156,11 +180,11 @@ static void __exit cleanup(void)
     int i;
 
     for (i = 0; i < 4; ++i) 
-        if (proc_files[i] != NULL) {
-            remove_proc_entry(proc_names[i], NULL);
+        if (char_dev[i] != 0) {
+            unregister_chrdev(char_dev[i], char_names[i]);
         }
 
-    printk(KERN_ALERT "proc files removed\n");
+    printk(KERN_ALERT "char files removed\n");
 }
 
 module_init(init);
